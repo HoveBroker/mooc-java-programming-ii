4e141d408f4313f8cb9a8d36773362965848970c
HoveBroker <HoveBeachBroker@gmail.com>
2024-09-07 00:25:33 +0100

    Update Book.java
    // -- Needed for the ability to print out 'Book'. --
    
    Without toString() method, it's not possible to print out the files lines using an Array / List.

--- part10-Part10_10.BooksFromFile/src/main/java/Book.java
+++ part10-Part10_10.BooksFromFile/src/main/java/Book.java
@@ -29,4 +29,9 @@ public class Book {
         return pageCount;
     }
 
+    @Override
+    public String toString(){
+        return this.name + " " + this.publishingYear + " " + this.pageCount + " " + this.author; // -- Needed for the ability to print out 'Book'. --
+    }
+
 }
